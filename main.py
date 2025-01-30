9d5fffa9c794309c8463b0b4326188627d5d267c
shade477 <sagardeyof477@gmail.com>
2025-01-30 20:17:03 +0530

    Added Maze class

--- main.py
+++ main.py
@@ -2,14 +2,36 @@ from Window import Window
 from line import Line
 from point import Point
 from cell import Cell
+from maze import Maze
 
 def main():
     win = Window(800, 600)
-    cell1 = Cell(1, 51, 1, 51, win)
-    cell2 = Cell(1,51, 51, 101, win)
-    cell1.draw()
-    cell2.draw()
-    cell1.draw_move(cell2)
+    # c1 = Cell(win)
+    # c1.has_right_wall = False
+    # c1.draw(50, 50, 100, 100)
+
+    # c2 = Cell(win)
+    # c2.has_left_wall = False
+    # c2.has_bottom_wall = False
+    # c2.draw(100, 50, 150, 100)
+
+    # c1.draw_move(c2)
+
+    # c3 = Cell(win)
+    # c3.has_top_wall = False
+    # c3.has_right_wall = False
+    # c3.draw(100, 100, 150, 150)
+
+    # c2.draw_move(c3)
+
+    # c4 = Cell(win)
+    # c4.has_left_wall = False
+    # c4.draw(150, 100, 200, 150)
+
+    # c3.draw_move(c4, True)
+
+    maze = Maze(1, 1, 5, 5, 50, 50, win)
+
     win.wait_for_close()
 
 if __name__ == '__main__':
---
+++ maze.py
@@ -0,0 +1,44 @@
+from cell import Cell
+import time
+
+class Maze:
+    def __init__(
+        self,
+        x1,
+        y1,
+        num_rows,
+        num_cols,
+        cell_size_x,
+        cell_size_y,
+        win,
+    ):
+        self._x1 = x1
+        self._y1 = y1
+        self._num_rows = num_rows
+        self._num_cols = num_cols
+        self._cell_size_x = cell_size_x
+        self._cell_size_y = cell_size_y
+        self._win = win
+        self._create_cells()
+        
+    
+    def _create_cells(self):
+        
+        self._cells = [[Cell(self._win) for _ in range(self._num_rows)] for _ in range(self._num_cols)]
+        for c in range(self._num_cols):
+            for r in range(self._num_rows):
+                self._draw_cell(c, r)
+
+
+    def _draw_cell(self, i, j):
+        x = self._x1 + (i + 1) * self._cell_size_x
+        y = self._y1 + (j + 1) * self._cell_size_y
+
+        # Change if num rows and cols mismatch
+        cell = self._cells[i][j]
+        cell.draw(x, y, x + self._cell_size_x, y + self._cell_size_y)
+        self._animate()
+
+    def _animate(self):
+        self._win.redraw()
+        time.sleep(0.05)
