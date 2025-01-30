59057baaf58ed903a5ebbe0a21a12e0063bcabd7
shade477 <sagardeyof477@gmail.com>
2025-01-30 13:33:04 +0530

    Added repr to Point

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from point import Point
 def main():
     win = Window(800, 600)
     l1 = Line(Point(525,325), Point(300,200))
-    win.draw_line(l1, 'white')
+    win.draw_line(l1, 'black')
     win.wait_for_close()
 
 if __name__ == '__main__':
